7114fc9f10f4e5ead2ae54bf513bab4eb0a64c17
Christoph Pickl <christoph.pickl@gmail.com>
2020-04-04 07:02:52 +0200

    add conversation cards

--- src/main/kotlin/com/github/christophpickl/cardz/source/Conversation.kt
+++ src/main/kotlin/com/github/christophpickl/cardz/source/Conversation.kt
@@ -2,10 +2,47 @@ package com.github.christophpickl.cardz.source
 
 /*
 by flow magazine
+
+these 32 cards feature questions that might set off a slightly different conversation
+with the people close to you than you're used to.
+you could also try answering the questions yourself;
+they could help you reflect on what you find important in life,
+how you spend your time and things you would like to change.
 */
 object Conversation : Source {
     override val fileTitle = "conversation"
     override val sentences = listOf(
-        "abc"
+        "What pops into your head when you hear the word 'waiting'?",
+        "Do you ever block a few hours in your schedule and spend them all on yourself?",
+        "What do you consider to be your greatest talent? Do you feel you use it enough?",
+        "What do you do to feel better when you're not having a great day?",
+        "What did you love doing when you were a child? Are you still into the same things?",
+        "When was the last time you spent a morning or afternoon not doing anything in particular? What was that like?",
+        "Which relationship in your life has had the most profound impact on you? Why was that?",
+        "What is the oldest item of clothing you own and still wear? What makes it special?",
+        "When did you last succumb to helpless laughter?",
+        "What is the thing that you worry about most?",
+        "Have you ever experienced a badly broken heart? Can you remember the moment that you realized that 'This too shall pass'?",
+        "What makes you relax?",
+        "Can you describe a time when your gut instinct turned out to be right?",
+        "Which three things would you like to spend more time on in your life? And less?",
+        "What do you look forward to as you get older?",
+        "If you compare your life now to your life ten years ago, what has changed for the better?",
+        "Which song do you currently play on repeat? Why is it significant for you at the moment?",
+        "Which conversation has had a big impact on your life? Why was that?",
+        "Imagine you're going on a solo weekend trip. Where would you go and what would you do?",
+        "Do you ever tak ea nap during the day?",
+        "Which of your expectations often lead to you feeling disappointed?",
+        "Of all the people in your life, which are the ones that are most important to you?",
+        "Who is the last real friend you made? What was it that you liked about each other?",
+        "Which set patterns in your life would you like to change if you could?",
+        "What would you like to be really good at?",
+        "Does the idea of not seeing or speaking to anyone for a week appeal to you?",
+        "What do you like doing most when you want to enjoy the outdoors?",
+        "Will you be doing the same job in five years' time? Or do you occasionally dream of something else?",
+        "Which of your guilty pleasures do you not even feel remotely guilty of?",
+        "Have you ever experienced a setback that, in retrospect, brough you further in life? What happened?",
+        "What do you feel is important in life?",
+        "Is there a household chore that you actually enjoy doing? Why do you like it?"
     )
 }
